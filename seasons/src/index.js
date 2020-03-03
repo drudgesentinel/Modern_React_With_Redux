fc629d1a0246e051fccb84878fb891b0d650b018
Kano <kano@wormydog.xyz>
2020-03-03 08:53:08 -0800

    componentDidMount

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,24 +4,23 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   //automatically called on class instance creation
+  //do not do data loading in the constructor; use componentDidMount method instead (best practices/readability)
   constructor(props) {
     //super is required
     super(props);
     //Only acceptable direct assignment to this.state is inside constructor
     this.state = { lat: null, errorMessage: '' };
-
-    window.navigator.geolocation.getCurrentPosition(
-  (position) => {
-    //use setState, not direct assignment except in constructor function
-    this.setState({ lat: position.coords.latitude});
-  },
-  (err) => {
-    this.setState({ errorMessage: err.message });
   }
+
+componentDidMount(){
+  window.navigator.geolocation.getCurrentPosition(
+  //use setState, not direct assignment except in constructor function
+  position => this.setState({ lat: position.coords.latitude}),
+err => this.setState({ errorMessage: err.message })
 );
-  }
-
+}
   //without render method, error will be thrown
+  //render is only for returning JSX
   render() {
     if(this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
